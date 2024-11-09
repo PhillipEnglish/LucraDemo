fbce3963668f712dc7df27245098af0068af05c0
Phillip English <vitkiarts@gmail.com>
2024-11-09 16:40:44 -0500

    Initial Commit

---
+++ LucraDemo/LucraDemo/LucraDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucraDemoApp.swift
+//  LucraDemo
+//
+//  Created by Phillip English on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LucraDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
