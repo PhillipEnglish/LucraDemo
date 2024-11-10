19b9c4b36c2ecc4f6dacdd99e474d9bad326ad11
Phillip English <vitkiarts@gmail.com>
2024-11-10 14:46:11 -0500

    Add custom color literal extension

---
+++ LucraDemo/LucraDemo/Extensions/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  LucraDemo
+//
+//  Created by Phillip English on 11/10/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let lucraGreen = Color(#colorLiteral(red: 0.102, green: 0.847, blue: 0.396, alpha: 1)) // #1ad865
+    static let lucraBlue = Color(#colorLiteral(red: 0.204, green: 0.231, blue: 0.498, alpha: 1)) // #343b7f
+    static let lucraRoyalBlue = Color(#colorLiteral(red: 0.004, green: 0.086, blue: 0.212, alpha: 1)) // #011636
+    static let lucraBlueWhite = Color(#colorLiteral(red: 0.843, green: 0.894, blue: 0.945, alpha: 1)) // #d7e4f1
+}
